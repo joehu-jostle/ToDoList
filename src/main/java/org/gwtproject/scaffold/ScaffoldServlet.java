6f958ace8f74ec8954ca928935a77ccd27d82e94
hujoe55 <hujoe55@gmail.com>
2017-05-04 09:55:45 -0700

    create a scaffold welcome message

---
+++ src/main/java/org/gwtproject/scaffold/ScaffoldServlet.java
@@ -0,0 +1,23 @@
+package org.gwtproject.scaffold;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Joe on 5/4/2017.
+ */
+public class ScaffoldServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Set response content type
+        resp.setContentType("text/html");
+
+        // Actual logic goes here.
+        PrintWriter out = resp.getWriter();
+        out.println("<h1> Welcome to the hybrid app.  This message was obtained from a HttpServlet that's not tied to GWT</h1>");
+    }
+}
